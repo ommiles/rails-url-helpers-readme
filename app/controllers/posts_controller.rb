178f9ba7525f34410e555209345a04cf90d45f8a
OM Miles <miles@ommiles.com>
2021-06-08 10:36:23 -0700

    lab complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		#  When you pass the :show argument to the resources method, 
+		# it will automatically create the show route 
+		# the show route will assume that you will need to pass the id into the URL string
+		# Whenever you have id parameters listed in the path like this, 
+		# you will need to pass the route helper method an ID
 		@post = Post.find(params[:id])
 	end
 end
